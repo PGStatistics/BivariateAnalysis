72b9a7f7c23e362f981f0953ce4a59ed18ce2fba
GrzegorzMeller <grzesek98@gmail.com>
2018-06-22 15:40:18 +0200

    Update on step analysis, chosen new coefficients for regression analysis

--- RegressionAlnalysis.R
+++ RegressionAlnalysis.R
@@ -1,14 +1,18 @@
-#https://stats.stackexchange.com/questions/76226/interpreting-the-residuals-vs-fitted-values-plot-for-verifying-the-assumptions
+#https://data.library.virginia.edu/diagnostic-plots/
 source('movies.R')
 library(Ecdat)
 
 powerTransform(clean.movies$revenue) #0.2277891
+pom<-lm(revenue~clean.movies$budget+clean.movies$runtime+clean.movies$popularity+clean.movies$vote_average+clean.movies$vote_count+clean.movies$original_language+clean.movies$production_countries,data=clean.movies)
+step(pom, direction="both")
+
 
 full<-lm(revenue^0.22~budget+runtime, data=clean.movies)
 summary(full)
 AIC(full)
 library(fmsb)
 VIF(full)
+par(mfrow=c(2,2))
 plot(full)
 exp(full$coefficients)
 
@@ -18,7 +22,7 @@ qqPlot(full$residuals)
 ncvTest(full)
 
 exp(clean.movies$revenue)
-step(full, direction="both")
+
 
 library(leaps)
 best.subset<-regsubsets(revenue^0.22~budget+runtime, data=clean.movies,nvmax=5) 
