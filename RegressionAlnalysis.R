03a8d0c4b809b4168326329e8a543073c6267724
GrzegorzMeller <grzesek98@gmail.com>
2018-06-22 15:41:53 +0200

    Update on RegressionAnalysis (last commit went wrong)

--- RegressionAlnalysis.R
+++ RegressionAlnalysis.R
@@ -3,11 +3,15 @@ source('movies.R')
 library(Ecdat)
 
 powerTransform(clean.movies$revenue) #0.2277891
-pom<-lm(revenue~clean.movies$budget+clean.movies$runtime+clean.movies$popularity+clean.movies$vote_average+clean.movies$vote_count+clean.movies$original_language+clean.movies$production_countries,data=clean.movies)
+pom<-lm(revenue~clean.movies$budget+clean.movies$runtime+clean.movies$popularity+clean.movies$vote_average+
+          clean.movies$vote_count+clean.movies$original_language+clean.movies$production_countries+
+          clean.movies$spoken_languages+clean.movies$release_date,data=clean.movies)
 step(pom, direction="both")
 
-
-full<-lm(revenue^0.22~budget+runtime, data=clean.movies)
+#after step anlysis we select following coefficients:
+full<-lm(formula = revenue^0.22 ~ clean.movies$budget +
+           clean.movies$runtime + clean.movies$popularity + clean.movies$vote_count + 
+           clean.movies$release_date, data = clean.movies)
 summary(full)
 AIC(full)
 library(fmsb)
@@ -21,11 +25,3 @@ crPlots(full)
 qqPlot(full$residuals)
 ncvTest(full)
 
-exp(clean.movies$revenue)
-
-
-library(leaps)
-best.subset<-regsubsets(revenue^0.22~budget+runtime, data=clean.movies,nvmax=5) 
-podsumowanie<-summary(best.subset)
-podsumowanie$outmat
-plot(best.subset, scale="adjr2")
