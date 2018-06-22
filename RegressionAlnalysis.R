d95f69b4680f456bcfcd1b980e8a3bc1ad77dd6b
GrzegorzMeller <grzesek98@gmail.com>
2018-06-22 17:17:25 +0200

    chosen the best model for regression analysis

--- RegressionAlnalysis.R
+++ RegressionAlnalysis.R
@@ -1,26 +1,29 @@
 #https://data.library.virginia.edu/diagnostic-plots/
-source('movies.R')
-library(Ecdat)
+source('movies.R') #remember to compile this before compiling RegressionAnalysis!
 
-powerTransform(clean.movies$revenue) #0.2277891
 pom<-lm(revenue~clean.movies$budget+clean.movies$runtime+clean.movies$popularity+clean.movies$vote_average+
           clean.movies$vote_count+clean.movies$original_language+clean.movies$production_countries+
-          clean.movies$spoken_languages+clean.movies$release_date,data=clean.movies)
+          clean.movies$spoken_languages+clean.movies$release_year,data=clean.movies)
 step(pom, direction="both")
 
 #after step anlysis we select following coefficients:
-full<-lm(formula = revenue^0.22 ~ clean.movies$budget +
-           clean.movies$runtime + clean.movies$popularity + clean.movies$vote_count + 
-           clean.movies$release_date, data = clean.movies)
+library(Ecdat)
+library(car)
+powerTransform(clean.movies$revenue) 
+full<-lm(formula = revenue^0.22 ~ clean.movies$budget + clean.movies$runtime + clean.movies$popularity + 
+           clean.movies$vote_count , data = clean.movies)
+par(mfrow=c(2,2))
+plot(full)
+
 summary(full)
 AIC(full)
 library(fmsb)
 VIF(full)
-par(mfrow=c(2,2))
-plot(full)
+
+
 exp(full$coefficients)
 
-library(car)
+
 crPlots(full)
 qqPlot(full$residuals)
 ncvTest(full)
